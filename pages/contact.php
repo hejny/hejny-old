b2c72d1460dbaeb295a086d8cd09007ef87af033
Pavol Hejný <ph@towns.cz>
2016-03-14 12:05:33 +0100

    update

---
+++ pages/contact.php
@@ -0,0 +1,9 @@
+
+
+<div id="contacts">
+    <a href="https://www.facebook.com/hejny" target="_blank"><i class="icon-social-facebook icons"></i></a>
+    <a href="https://twitter.com/pavolhejny" target="_blank"><i class="icon-social-twitter icons"></i></a>
+    <a href="https://www.linkedin.com/profile/view?id=AAMAABL-alkBCN2nJuDO_lhg0Pg0H47UcYbeYgM&trk=hp-identity-photo" target="_blank"><i class="icon-social-linkedin icons"></i></a>
+    <a href="mailto:ph@towns.cz" target="_blank"><i class="icon-envelope icons"></i></a>
+</div>
+
