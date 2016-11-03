3e7fbd6179a767773fd7e123a74eeda8c74d6dfc
Pavol Hejný <me@pavolhejny.cz>
2016-11-03 15:13:21 +0100

    updates before GDS 2016

--- pages/contact.php
+++ pages/contact.php
@@ -4,7 +4,7 @@
 </a>
 
 <div id="contacts">
-    <a href="mailto:ph@towns.cz" target="_blank"><i class="icon-envelope icons"></i></a>
+    <a href="mailto:me@pavolhejny.com" target="_blank"><i class="icon-envelope icons"></i></a>
     <a href="https://twitter.com/pavolhejny" target="_blank"><i class="icon-social-twitter icons"></i></a>
     <a href="https://www.facebook.com/hejny" target="_blank"><i class="icon-social-facebook icons"></i></a>
     <a href="https://www.linkedin.com/profile/view?id=AAMAABL-alkBCN2nJuDO_lhg0Pg0H47UcYbeYgM&trk=hp-identity-photo" target="_blank"><i class="icon-social-linkedin icons"></i></a>
